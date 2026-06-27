15c1709142d7329fb06e5bdd3406e2d50ec5d093
Mohammad Ausaf <166019353+AusafG5@users.noreply.github.com>
2026-06-28 02:09:35 +0530

    Update Searchy cask to 4.3

--- Casks/searchy.rb
+++ Casks/searchy.rb
@@ -1,8 +1,9 @@
 cask "searchy" do
-  version "4.2"
-  sha256 "ace581614ca499ddb2daf27c00311fbaf7341b000cbe943c672fc95451ece254"
+  version "4.3"
+  sha256 "92a41b835399a77baf9de47fe1f85e769176e8a86e182d8ee5215ca718604bb4"
 
-  url "https://github.com/AusafMo/searchy/releases/download/v#{version}/Searchy-v#{version}.dmg"
+  url "https://github.com/AusafMo/searchy/releases/download/v#{version}/Searchy-v#{version}.dmg",
+      verified: "github.com/AusafMo/searchy/"
   name "Searchy"
   desc "Hybrid image search tool for macOS using CLIP and OCR"
   homepage "https://ausafmo.com/searchy.html"
@@ -13,7 +14,7 @@ cask "searchy" do
   end
 
   auto_updates true
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "searchy.app"
 
